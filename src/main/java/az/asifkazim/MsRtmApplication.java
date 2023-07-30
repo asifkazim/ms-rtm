745b8c3b0bf044257ce95fd835d637fab363c9be
asifkazim <asifkazimov98@gmail.com>
2023-07-30 16:36:54 +0400

    initial commit

---
+++ src/main/java/az/asifkazim/MsRtmApplication.java
@@ -0,0 +1,13 @@
+package az.asifkazim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRtmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsRtmApplication.class, args);
+    }
+
+}
